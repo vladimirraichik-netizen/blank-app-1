e00cd8f9249a650dee4c9c61b76c6559d1eea42a
vladimirraichik-netizen <vladimir.raichik@gmail.com>
2026-09-07 19:59:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -114,7 +114,6 @@ with col1:
         ax1.set_ylim(-9, 9)
         
     else:
-        # Задаем красивое отображение через радикалы для функций и их обратных
         if p == 1 and q == 2:
             power_str = "y = \\sqrt{x}"
             inv_str = "y = x^2"
@@ -198,7 +197,8 @@ with col2:
     ax2.axhline(0, color='black', lw=1.2, zorder=1)
     ax2.axvline(0, color='black', lw=1.2, zorder=1)
     ax2.plot(x_exp, y2_exp, lw=2.2, color='#d62728', label=f'$y = {a_gt1:.2f}^{{x}}$', zorder=3)
-    ax2.plot(x_log, y2_log, lw=2.2, color='#2ca02c', label=f'$y = \\log_{{{a_gt1:.2f}}}(x)$', zorder=3)
+    # Исправленный формат логарифма: основание внизу, аргумент без скобок
+    ax2.plot(x_log, y2_log, lw=2.2, color='#2ca02c', label=f'$y = \\log_{{{a_gt1:.2g}}} x$', zorder=3)
     ax2.plot(x_log, x_log, color='gray', linestyle='--', lw=1.3, label='$y = x$', zorder=2)
     
     ax2.grid(True, linestyle=':', alpha=0.7, zorder=0)
@@ -221,7 +221,8 @@ with col3:
     ax3.axhline(0, color='black', lw=1.2, zorder=1)
     ax3.axvline(0, color='black', lw=1.2, zorder=1)
     ax3.plot(x_exp, y3_exp, lw=2.2, color='#d62728', label=f'$y = {a_lt1:.2f}^{{x}}$', zorder=3)
-    ax3.plot(x_log, y3_log, lw=2.2, color='#2ca02c', label=f'$y = \\log_{{{a_lt1:.2f}}}(x)$', zorder=3)
+    # Исправленный формат логарифма: основание внизу, аргумент без скобок
+    ax3.plot(x_log, y3_log, lw=2.2, color='#2ca02c', label=f'$y = \\log_{{{a_lt1:.2g}}} x$', zorder=3)
     ax3.plot(x_log, x_log, color='gray', linestyle='--', lw=1.3, label='$y = x$', zorder=2)
     
     ax3.grid(True, linestyle=':', alpha=0.7, zorder=0)
