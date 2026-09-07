974b45c8fd1c0657e758cc67a41271b135740696
vladimirraichik-netizen <vladimir.raichik@gmail.com>
2026-09-07 11:37:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
+import subprocess
+import sys
+
+try:
+    import matplotlib
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "matplotlib", "numpy"])
+
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
